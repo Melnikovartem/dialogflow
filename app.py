f0f26cdfacbe9a2870b7b27ae4d7d78b59b0e7a4
hi_melnikov <melnikovam10@gmail.com>
2019-08-02 14:16:06 +0300

    restful flask not useful here

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
+api = Api(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'working'
+
+@app.route('/dialog')
+def ProccessGoogleRequest():
+    req = request.get_json()
+    logging.debug(request.get_json())
+    response = ''
+    return {"speech": response, "displayText": response}
+
 
 if __name__ == "__main__":
     app.run()
